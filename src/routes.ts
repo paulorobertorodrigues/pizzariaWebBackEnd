d7ec5134a0a80eaa43ff785c44ce13dd03250b82
paulorobertorodrigues <pauloroberto23061985@gmail.com>
2024-11-26 19:33:51 -0300

    :construction:Criando Middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
+
 import { CreateUserController } from './controllers/user/CreateUserControllers';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthenticated } from './middlewares/isAuthenticated';
+
 
 const router = Router();
 
 // Rotas USER
 router.post('/users', new CreateUserController().handle)
+
 router.post('/session', new AuthUserController().handle)
+
+router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 export { router };
--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from 'jsonwebtoken';
 
 interface AuthRequest{
     email: string;
@@ -25,9 +26,25 @@ class AuthUserService{
             throw new Error("User/password incorrect")
         }
 
-        //gerar um token JWT e devolver os dados do usuário com id , name e email
+        //se deu tudo certo vamos gerar o token pro usuário
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
 
-        return { ok: true}
+        return{
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
